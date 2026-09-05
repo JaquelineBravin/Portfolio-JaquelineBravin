d9a6737f95307d18b0bf9366d1b5e91132363990
JaquelineBravin <jaqbravin@hotmail.com>
2026-09-05 18:18:24 -0400

    feature: design; reponsive design;

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,67 +1,76 @@
 import './Home.css';
+import perfilImg from '../../../img/perfil.png';
 
 function Home() {
   return (
     <>
       <div className="home">
-        <div className="home-content">
-          <h1>Welcome!</h1>
-          <p>Hi there,</p>
-          <p>
-            I'm Jaqueline, a front and back-end developer, with projects in
-            JavaScript, TypeScript, NodeJs, ReactJs, databases and a few more...
-            I worked 4 years as a technical support in an internet company,
-            where I leard A LOT.
-          </p>
-          <p>
-            Currently I am in my last year of my Barchelor's degree in
-            Information Systems, aaand open to work! Feel free to check my
-            projects and/or contact me! ;)
-          </p>
-        </div>
-        <div className="home-projects">
-          <h3>Projects</h3>
-          <div className="home-projects-list">
-            <a
-              target="_blank"
-              href="https://github.com/JaquelineBravin/CrudComBancoDeDados-NodeJs.git"
-            >
-              NodeJs API com CRUD
-            </a>
-            <a
-              target="_blank"
-              href="https://github.com/JaquelineBravin/GithubUsers-JavaScript.git"
-            >
-              Github Users
-            </a>
-            <a
-              target="_blank"
-              href="https://github.com/JaquelineBravin/MovieApp-JavaScript.git"
-            >
-              TMDB MovieApp
-            </a>
-            <a
-              target="_blank"
-              href="https://github.com/JaquelineBravin/Microsservico-NodeJS.git"
-            >
-              Microsserviço NodeJs
-            </a>
-            <a
-              target="_blank"
-              href="https://github.com/JaquelineBravin/GeradorDePodcasts-NodeJS.git"
-            >
-              Manager de Podcast
-            </a>
-            <a></a>
-            <a></a>
-            <a></a>
-            <a
-              target="_blank"
-              className="see-more"
-              href="https://github.com/JaquelineBravin"
-            >
-              See more
-            </a>
+        <div className="home-container">
+          <div className="home-content">
+            <div className="profile">
+              <img src={perfilImg} alt="Perfil" />
+            </div>
+            <div className="home-bio">
+              <h1>Welcome!</h1>
+              <p>Hi there,</p>
+              <p>
+                I'm Jaqueline, a front and back-end developer, with projects in
+                JavaScript, TypeScript, NodeJs, ReactJs, databases and a few
+                more... I worked 4 years as a technical support in an internet
+                company, where I leard A LOT.
+              </p>
+              <p>
+                Currently I am in my last year of my Barchelor's degree in
+                Information Systems, aaand open to work! Feel free to check my
+                projects and/or contact me! ;)
+              </p>
+            </div>
+          </div>
+
+          <div className="home-projects">
+            <h3>Projects</h3>
+            <div className="home-projects-list">
+              <a
+                target="_blank"
+                href="https://github.com/JaquelineBravin/CrudComBancoDeDados-NodeJs.git"
+              >
+                NodeJs API com CRUD
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/JaquelineBravin/GithubUsers-JavaScript.git"
+              >
+                Github Users
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/JaquelineBravin/MovieApp-JavaScript.git"
+              >
+                TMDB MovieApp
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/JaquelineBravin/Microsservico-NodeJS.git"
+              >
+                Microsserviço NodeJs
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/JaquelineBravin/GeradorDePodcasts-NodeJS.git"
+              >
+                Manager de Podcast
+              </a>
+              <a></a>
+              <a></a>
+              <a></a>
+              <a
+                target="_blank"
+                className="see-more"
+                href="https://github.com/JaquelineBravin"
+              >
+                See more
+              </a>
+            </div>
           </div>
         </div>
       </div>
